61644a8da88fa46033a32ef0de578a47a3e4ea2b
muronjixxl <muronjiii@gmail.com>
2021-08-03 08:58:56 +0300

    adds the hover effect in the portfolio

--- js/script.js
+++ js/script.js
@@ -1,97 +1,52 @@
 $(document).ready(function () {
   $("#development-image").click(function () {
-    $("#development-image").slideDown('1500').hide('1000');
-    $("#development").show('1500');
+    $("#development-image").slideDown().hide();
+    $("#development").show(1000);
   });
   $("#development").click(function () {
-    $("#development").slideUp('1500');
-    $("#development-image").slideDown('1500');
+    $("#development").slideUp();
+    $("#development-image").slideDown();
   });
 });
 $(document).ready(function () {
   $("#design-image").click(function () {
-    $("#design-image").slideDown('1500').hide('1000');
-    $("#design").show('1500');
+    $("#design-image").slideDown().hide();
+    $("#design").show(1000);
   });
   $("#design").click(function () {
-    $("#design").slideUp('1500');
-    $("#design-image").slideDown('1500');
+    $("#design").slideUp();
+    $("#design-image").slideDown();
   });
 });
 $(document).ready(function () {
   $("#product-image").click(function () {
-    $("#product-image").slideDown('1500').hide('1000');
-    $("#product").show('1500');
+    $("#product-image").slideDown().hide();
+    $("#product").show(1000);
   });
   $("#product").click(function () {
-    $("#product").slideUp('1500');
-    $("#product-image").slideDown('1500');
+    $("#product").slideUp();
+    $("#product-image").slideDown();
   });
 });
+$(".image-overlay").hover(function () {
+  $(".image-title").css("border", "1px solid", "white");
 
-$(`.portfolio1`).hover(() => {
-  $(`.overlay1`).show(800);
-},
-  () => {
-    $(`.overlay1`).hide(800);
-  }
-)
+  $(".image-title").css("border-radius", "2px");
+  $(".image-title").css("padding", "5px");
+});
+$(document).ready(function () {
+  $(".text").hover(
+    function () {
+      $(this).animate({ opacity: "1" });
+    },
+    function () {
+      $(this).animate({ opacity: "0" });
+    }
+  );
+});
+});
 
-$(`.portfolio2`).hover(() => {
-  $(`.overlay2`).show(800);
-},
-  () => {
-    $(`.overlay2`).hide(800);
-  }
-)
 
-$(`.portfolio3`).hover(() => {
-  $(`.overlay3`).show(800);
-},
-  () => {
-    $(`.overlay3`).hide(800);
-  }
-)
-
-$(`.portfolio4`).hover(() => {
-  $(`.overlay4`).show(800);
-},
-  () => {
-    $(`.overlay4`).hide(800);
-  }
-)
-
-$(`.portfolio5`).hover(() => {
-  $(`.overlay5`).show(800);
-},
-  () => {
-    $(`.overlay5`).hide(800);
-  }
-)
-
-$(`.portfolio6`).hover(() => {
-  $(`.overlay6`).show(800);
-},
-  () => {
-    $(`.overlay6`).hide(800);
-  }
-)
-
-$(`.portfolio7`).hover(() => {
-  $(`.overlay7`).show(800);
-},
-  () => {
-    $(`.overlay7`).hide(800);
-  }
-)
-
-$(`.portfolio8`).hover(() => {
-  $(`.overlay8`).show(800);
-},
-  () => {
-    $(`.overlay8`).hide(800);
-  }
-)
 
 let validateInput = () => {
   if ($("#name").val() == "") {
